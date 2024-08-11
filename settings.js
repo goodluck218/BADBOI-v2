33d051bc6131c1275f6158560b0ef05437f99cb0
goodluck218 <goodluckmaduabuchi92@gmail.com>
2024-08-11 20:55:04 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,13 +4,13 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "2348140825959"
+global.ownernomer = "2348171902471"
 global.ownername = "🕷BADBOI-v2-BUG🕷"
 global.ytname = "🕷BADBOI-v2-BUG🕷"
 global.socialm = "GitHub: BADBOI-v1"
 global.location = "Africa, Nigeria, Ogun-state"
 
-global.ownernumber = '2348140825959,2348140825959,2348140825959'  //creator number
+global.ownernumber = '2348171902471,2348140825959,2348140825959'  //creator number
 global.ownername = '🕷BADBOI-v2-BUG🕷' //owner name
 global.botname = '🕷BADBOI-v2-BUG🕷' //name of the bot
 
